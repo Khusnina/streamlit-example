d3bb36c9697627790b36b1cd2b1efded19688b4e
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-13 12:18:06 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,3 +6,5 @@ st.subheader("An extractive text summary system that generates summaries for a l
 st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
 st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 
+box1= Entry(base)  
+box1.place(x=200, y=120)  
