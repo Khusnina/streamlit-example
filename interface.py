926653f7fd169e4ae6ea3a63ebb4cfccdc0fca0d
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-13 12:08:24 +0800

    Delete interface.py

--- interface.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
-
-st.subheader("An extractive text summary system that generates summaries for a large number of texts.:wave:")
-st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
-st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
-
