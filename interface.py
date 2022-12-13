f07c7f23e63f51a62dcc1c1e9ae2ad07cb4af2a0
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-13 12:29:40 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from tkinter import *
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 
@@ -7,6 +6,7 @@ st.subheader("An extractive text summary system that generates summaries for a l
 st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
 st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 
-base = Tk()
-box1= Entry(base)  
-box1.place(x=200, y=120)  
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
