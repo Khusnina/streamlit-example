25867430de1a066d50ec0ba14aa98315bf8080c6
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:11:37 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 def main():
-	st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
+   st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 
-   	st.subheader("An extractive text summary system that generates summaries for a large number of texts")
-   	st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
+   st.subheader("An extractive text summary system that generates summaries for a large number of texts")
+   st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
    	
-	st.title("TEXT SUMMARIZER")
-	st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
-	activities = ["Summarize"]
-	choice = st.sidebar.selectbox("Select Activity",activities)
+   st.title("TEXT SUMMARIZER")
+   st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
+   activities = ["Summarize"]
+   choice = st.sidebar.selectbox("Select Activity",activities)
 	
 with st.form("my_form"):
    st.write("Upload TXT File")
