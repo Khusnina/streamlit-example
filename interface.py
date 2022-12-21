d7731d3d1a0f7abeabdb4605e5f4173277bf1a31
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:15:20 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-   st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
-   st.subheader("An extractive text summary system that generates summaries for a large number of texts")
-   st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
-   st.title("TEXT SUMMARIZER")
-   st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
+st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
+st.subheader("An extractive text summary system that generates summaries for a large number of texts")
+st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
+st.title("TEXT SUMMARIZER")
+st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
    
 with st.form("my_form"):
    st.write("Upload TXT File")
