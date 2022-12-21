b6b6fac5838033b5aeee80f16cac8c0027ff5f6c
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:31:11 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,16 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES\n..............................................................................................................................................................................................")
 st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
-st.title("TEXT SUMMARIZER")
-activities = ["Summarize"]
+activities = ["Information","Summarize"]
 choice = st.sidebar.selectbox("Select Activity",activities)   
 
-
-with st.form("my_form"):
-   st.write("Upload TXT File")
-   title = st.text_input('Fiction Book Title')
-   st.write('Fiction Books Content', title)
-   st.form_submit_button("Summarize")
+if choice == 'Summarize':
+   st.subheader("TEXT SUMMARIZER")
+   raw_text = st.text_area("Enter Text Here","Text Content")
 
    
    
