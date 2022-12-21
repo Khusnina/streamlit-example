d68c54ab573f1ad4d1b64855cef3eb0bd12f66a9
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:42:33 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,24 +6,15 @@ app = Flask(__name__)
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES\n..............................................................................................................................................................................................")
-st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 activities = ["Information","Summarize"]
 choice = st.sidebar.selectbox("Select Activity",activities)   
+
+if choice == 'Information'
+   st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 
 if choice == 'Summarize':
    st.subheader("TEXT SUMMARIZER")
    raw_text = st.text_area("Enter Text Here","Text Content")
-  
-   @app.route('/')  
-   def main():  
-      return render_template("index.html")  
-  
-   @app.route('/success', methods = ['POST'])  
-   def success():  
-      if request.method == 'POST':  
-         f = request.files['file']
-         f.save(f.filename)  
-         return render_template("Acknowledgement.html", name = f.filename)  
   
 if __name__ == '__main__':  
     app.run(debug=True)
