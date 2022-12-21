d1e9c1989f152191333ff61f6420fdbd382b1cb9
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:17:40 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
-st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES\n........................................................................................................")
+st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES\n..................................................................................................................................................................................................................")
 st.title("TEXT SUMMARIZER")
 st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 st.title("TEXT SUMMARIZER")
