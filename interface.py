c725c2aa77cd24e32af37dc473527a2fbe5b421f
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:43:26 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,8 +1,4 @@
-import streamlit as st
-from distutils.log import debug
-from fileinput import filename
-from flask import *  
-app = Flask(__name__)  
+import streamlit as st  
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES\n..............................................................................................................................................................................................")
@@ -15,9 +11,6 @@ if choice == 'Information'
 if choice == 'Summarize':
    st.subheader("TEXT SUMMARIZER")
    raw_text = st.text_area("Enter Text Here","Text Content")
-  
-if __name__ == '__main__':  
-    app.run(debug=True)
    if st.button("Summarize"):
       st.write(raw_text)
    
