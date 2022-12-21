1314d839f58aa9246e191b2957475c218ad38bba
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-21 21:09:49 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,23 +1,20 @@
 import streamlit as st
 
-st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
+def main():
+	st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 
-st.subheader("An extractive text summary system that generates summaries for a large number of texts")
-st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
-st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
-
+   	st.subheader("An extractive text summary system that generates summaries for a large number of texts")
+   	st.title("ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES")
+   	
+	st.title("TEXT SUMMARIZER")
+	st.write("he extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
+	activities = ["Summarize"]
+	choice = st.sidebar.selectbox("Select Activity",activities)
+	
 with st.form("my_form"):
    st.write("Upload TXT File")
    title = st.text_input('Fiction Book Title')
    st.write('Fiction Books Content', title)
    st.form_submit_button("Summarize")
 
-#custom funtion 
-def summary(text):
-    return summarize(text)
-
-# Custom Components Fxn
-def st_calculator(calc_html,width=1000,height=1350):
-	calc_file = codecs.open(calc_html,'r')
-	page = calc_file.read()
-	components.html(page,width=width,height=height,scrolling=False)
+   
