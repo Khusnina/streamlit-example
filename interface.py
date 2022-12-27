d6f32a1c92872d4da46e5487ccca581c7dac03a5
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-27 16:32:37 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import streamlit as st  
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
-st.title("<center> ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES <center>\n..............................................................................................................................................................................................")
+st.title("<center> ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES </center>\n..............................................................................................................................................................................................")
 activities = ["Introduction","Fiction Books","Summarize","Statistic"]
 choice = st.sidebar.selectbox("Select Activity",activities)   
 
