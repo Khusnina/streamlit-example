8501258513e1a1eaa8660b9c5c4ba48999907b33
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-27 22:17:06 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h2>", unsafe_allow_html=True)
-st.markdown("<h2 style='center;'>\n........................................................................................................</h1>", unsafe_allow_html=True)
+st.markdown("<hr width='90%;'></hr>", unsafe_allow_html=True)
 activities = ["Introduction","Fiction Books","Summarize","Statistic"]
 choice = st.sidebar.selectbox("Select Activity",activities)   
 
