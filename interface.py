cdbf2d73949986c2ef3326df2646fe39980fa9b0
Khusnina <116963275+Khusnina@users.noreply.github.com>
2022-12-28 16:57:54 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,13 +8,13 @@ st.markdown("<hr size='5' width='100%;'>", unsafe_allow_html=True)
 activities = ["Introduction","Fiction Books","Summarize","Statistic"]
 choice = st.sidebar.selectbox("Select Activity",activities)   
 Df = pd.read_csv("Khusnina/streamlit-example/listBook.csv", encoding="latin-1")
+st.write(Df)
 
 if choice == 'Introduction':
    st.write("The extractive text summarization system creates summaries by identifying, extracting the sentences, and subsequently combining the most important sentences in an online book to generate in form of a summary.")
 
 if choice == 'Fiction Books':
    st.markdown("<h2 style='text-align: center; color: white;'>FICTIONS BOOKS</h2>", unsafe_allow_html=True)
-   st.write(Df)
    df = pd.DataFrame(
    np.random.randn(15, 2),
    columns=('col %d' % i for i in range(2)))
