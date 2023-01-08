ce48e113e285aff157388935df48759d0e700f8a
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-09 00:24:25 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -44,10 +44,8 @@ if choice == 'Fiction Books':
       st.write("List of Fiction Book after cleaning")
       st.write(df.head(20))
       @st.cache
-      def convert_df(df):
-      # IMPORTANT: Cache the conversion to prevent computation on every rerun
-      return df.to_csv().encode('utf-8')
-      csv = convert_df(df)
+      convert_df = df.to_csv().encode('utf-8')
+      csv = convert_df
       st.download_button(
          label = "Download data in CSV",
          data = csv,
