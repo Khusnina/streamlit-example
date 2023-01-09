73614ac600273db7928db131c47f3c304a7f18a9
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-09 10:04:49 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -117,27 +117,30 @@ if choice == 'Fiction Books':
    
 if choice == 'Summarize':
    st.subheader("EXTRACTIVE TEXT SUMMARIZER")
-   st.button("Upload File (txt)")
    agree = st.checkbox('Show sentence')
-   raw_text = st.text_area("Original Content","Enter text here")
-   uploaded_file = st.file_uploader("Choose a file")
-   if uploaded_file is not None:
-      # To read file as bytes:
-      bytes_data = uploaded_file.getvalue()
-      st.write(bytes_data)
-      # To convert to a string based IO:
-      stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-      st.write(stringio)
-      # To read file as string:
-      string_data = stringio.read()
-      st.write(string_data)
-      # Can be used wherever a "file-like" object is accepted:
-      dataframe = pd.read_csv(uploaded_file)
-      st.write(dataframe)
+   with st.form(key = 'nlpForm')
+      raw_text = st.text_area("Original Content","Enter text here")
+      uploaded_file = st.file_uploader("Choose a file")
+      if uploaded_file is not None:
+         # To read file as bytes:
+         bytes_data = uploaded_file.getvalue()
+         st.write(bytes_data)
+         # To convert to a string based IO:
+         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+         st.write(stringio)
+         # To read file as string:
+         string_data = stringio.read()
+         st.write(string_data)
+         # Can be used wherever a "file-like" object is accepted:
+         dataframe = pd.read_csv(uploaded_file)
+         st.write(dataframe)
+      submit_button = st.form_submit_button(label = 'Summarize')
+   """
    if st.button("Summarize"):
       st.write(raw_text)
       st.button("Copy text")
       st.write("Words:")
+   """
       
    if agree:
     st.write('---')
