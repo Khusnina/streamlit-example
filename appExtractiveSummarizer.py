86921fa9ab1ff09d960d9397bc391831b9300c69
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 15:53:43 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -2,11 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
+import unidecode
+import re
+import time 
+import string
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer 
+from autocorrect import Speller 
+from bs4 import BeautifulSoup 
+from nltk import word_tokenize
+from tqdm import tqdm
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h1>", unsafe_allow_html=True)
