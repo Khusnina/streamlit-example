0fab7d3924ad760a1952d88fb71302ff811d4ac7
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 15:33:44 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -10,8 +10,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
 from nltk.stem import WordNetLemmatizer
 from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from tqdm import tqdm
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h1>", unsafe_allow_html=True)
