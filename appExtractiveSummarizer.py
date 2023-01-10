e2c96ab60fe15b4a8c2f2fca15131355da0963bf
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 11:59:46 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import io
 import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from nltk.stem import WordNetLemmatizer
