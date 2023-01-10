e53347ddcd40042807b45bbafc1552fff36ef71e
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 15:31:15 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -13,7 +13,6 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from tqdm import tqdm
-from PIL import Image
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h1>", unsafe_allow_html=True)
