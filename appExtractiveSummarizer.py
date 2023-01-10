ccd59c421b5b7267d934da1237920322ba0753f4
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 11:43:06 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -20,7 +20,7 @@ choice = st.sidebar.selectbox("Select Activity", activities)
 if choice == 'Introduction':
    st.markdown("<h2 style='text-align: center; color: white;'>INTRODUCTION</h2>", unsafe_allow_html=True)
    image = Image.open('summarization.png')
-   col1, col2, col3 = st.columns(3)
+   col1, col2, col3 = st.columns([1,10,1])
    with col1:
       st.write("")
    with col2:
