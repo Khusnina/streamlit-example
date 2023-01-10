a60f34c8d9aee8e6fcf024cfed9f2013331ae426
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-10 14:33:58 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -15,7 +15,8 @@ from PIL import Image
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h1>", unsafe_allow_html=True)
 st.markdown("<hr size='5' width='100%;'>", unsafe_allow_html=True)
-activities = ["Introduction",icon = "📚","Fiction Books","Summarize","Statistic"]
+activities = ["Introduction","📚 Fiction Books","Summarize","Statistic"]
+
 choice = st.sidebar.selectbox("Select Activity", activities)
 
 if choice == 'Introduction':
