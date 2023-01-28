40652e3538d7ad8f7988bc46674e072c643c215e
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-01-28 09:54:34 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -492,6 +492,8 @@ if choice == '📝 Summarize':
          countOfWords15 = len(Df['Description'][14].split())
          
          # Cache the dataframe so it's only loaded once
+         """
+         @st.experimental_memo
          def load_data():
             return pd.DataFrame(
                {
@@ -500,15 +502,29 @@ if choice == '📝 Summarize':
                }
             )
          
-         # Boolean to resize the dataframe, stored as a session state variable
-         st.checkbox("Use container width", value=False, key="use_container_width")
-         
          df = load_data()
 
          # Display the dataframe and allow the user to stretch the dataframe
          # across the full width of the container, based on the checkbox value
          st.dataframe(df, use_container_width=st.session_state.use_container_width)
-         
+         """
+         @st.experimental_memo
+         def load_data():
+            return pd.DataFrame(
+               {
+                  "Original Content": [Df['Description'][0], Df['Description'][1], Df['Description'][2], Df['Description'][3], Df['Description'][4], Df['Description'][5], Df['Description'][6], Df['Description'][7], Df['Description'][8], Df['Description'][9], Df['Description'][10], Df['Description'][11], Df['Description'][12], Df['Description'][13], Df['Description'][14]],
+                  "Summary": [str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, str13, str14, str15],
+               }
+            )
+
+         # Boolean to resize the dataframe, stored as a session state variable
+         st.checkbox("Use container width", value=False, key="use_container_width")
+
+         df = load_data()
+
+         # Display the dataframe and allow the user to stretch the dataframe
+         # across the full width of the container, based on the checkbox value
+         st.dataframe(df, use_container_width=st.session_state.use_container_width)
 if choice == '📊 Result':
    st.info("Result (TXT file)")
    st.info("Result (CSV file)")
