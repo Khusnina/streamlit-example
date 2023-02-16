9b5e8c63a6e51d2207716fc9064db3455ef34652
Khusnina <116963275+Khusnina@users.noreply.github.com>
2023-02-17 07:51:01 +0800

    Update appExtractiveSummarizer.py

--- appExtractiveSummarizer.py
+++ appExtractiveSummarizer.py
@@ -262,6 +262,8 @@ if choice == '📝 Summarize':
          Dftxt = uploaded_txt.read()
          raw_text = str(Dftxt,"utf-8")
          st.text(raw_text)
+         countOfWords = len(raw_text.split())
+         st.write("Count of Words: ", countOfWords)
       if st.button('Summarize file'):
          st.info("Results")
          contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not",
